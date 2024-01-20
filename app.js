bc57d017bcd349cee2682f1972dbe3f71650fb4d
Will Matheus <119905167+OWillMatheuz@users.noreply.github.com>
2024-01-20 20:45:09 -0300

    versão 100 numeros

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let listaDenumerosSorteados = [];
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
   let campo = document.querySelector(tag);
   campo.innerHTML = texto;
-  responsiveVoice.speak(texto, "Brazilian Portuguese Female", { rate: 1.2 });
 }
 
 function exibirMensagemInicial() {
   exibirTextoNaTela("h1", "Jogo do número secreto");
-  exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
+  exibirTextoNaTela("p", "Escolha um número entre 1 e " + numeroLimite);
 }
 
 exibirMensagemInicial();
@@ -38,16 +37,16 @@ function verificarChute() {
 
 function gerarNumeroAleatorio() {
   let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
-  let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
-
+  let quantidadeDeElementosNaLista = listaDenumerosSorteados.length;
   if (quantidadeDeElementosNaLista == numeroLimite) {
-    listaDeNumerosSorteados = [];
+    listaDenumerosSorteados = [];
   }
-  if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+
+  if (listaDenumerosSorteados.includes(numeroEscolhido)) {
     return gerarNumeroAleatorio();
   } else {
-    listaDeNumerosSorteados.push(numeroEscolhido);
-    console.log(listaDeNumerosSorteados);
+    listaDenumerosSorteados.push(numeroEscolhido);
+    console.log(listaDenumerosSorteados);
     return numeroEscolhido;
   }
 }
